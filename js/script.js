39baa6f347aee4cbe65bc8cf9448e787794288bf
pdecloud98 <pdecloud98@gmail.com>
2026-04-23 11:51:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log("Website loaded");
+const text = ["Data Analyst", "Data Scientist", "ML Enthusiast"];
+let index = 0;
+let charIndex = 0;
+let currentText = "";
+let isDeleting = false;
+
+function type() {
+    if (index >= text.length) index = 0;
+
+    currentText = text[index];
+
+    if (!isDeleting) {
+        document.getElementById("typing").innerText =
+            currentText.substring(0, charIndex++);
+    } else {
+        document.getElementById("typing").innerText =
+            currentText.substring(0, charIndex--);
+    }
+
+    if (!isDeleting && charIndex === currentText.length) {
+        isDeleting = true;
+        setTimeout(type, 1000);
+        return;
+    }
+
+    if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        index++;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
