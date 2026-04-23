e4ed91655f6a81e0450287155d7a5d10592d1564
pdecloud98 <pdecloud98@gmail.com>
2026-04-23 12:18:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,42 @@ function type() {
         index++;
     }
 
-    setTimeout(type, isDeleting ? 50 : 100);
+    setTimeout(type, 1500);
 }
 
 type();
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for (let i = 0; i < 80; i++) {
+    particles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        r: Math.random() * 2,
+        dx: Math.random() - 0.5,
+        dy: Math.random() - 0.5
+    });
+}
+
+function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach(p => {
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
+        ctx.fillStyle = "#4f46e5";
+        ctx.fill();
+
+        p.x += p.dx;
+        p.y += p.dy;
+    });
+
+    requestAnimationFrame(animateParticles);
+}
+
+animateParticles();
